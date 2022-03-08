64ebd99a0e2c7ceddbeae24cdf4c83c4f3e10271
Empereur-Franc <tazi.layt@gmail.com>
2022-03-08 12:03:30 +0100

    Déplacement ennemi (en cours)

--- ProjetTD/Assets/_003_Scripts/EnnemiMove.cs
+++ ProjetTD/Assets/_003_Scripts/EnnemiMove.cs
@@ -1,22 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class EnnemiMove : MonoBehaviour
 {
     public float _speed;
     public PointDirection PDirec;
+    private int PointDirectionIndex;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        PDirec = GameObject.FindGameObjectWithTag("PointsDirection").GetComponent<PointDirection>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        transform.position = Vector2.MoveTowards(transform.position, PDirec.pointsDirection[PointDirectionIndex].position, _speed * Time.deltaTime);
     }
 
 
