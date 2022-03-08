4eb5e261b36cf48a73be785b1f6a510dff632ea2
Empereur-Franc <tazi.layt@gmail.com>
2022-03-08 12:23:29 +0100

    Déplacement ennemi V1

--- ProjetTD/Assets/_003_Scripts/EnnemiMove.cs
+++ ProjetTD/Assets/_003_Scripts/EnnemiMove.cs
@@ -6,7 +6,7 @@ using UnityEngine.EventSystems;
 public class EnnemiMove : MonoBehaviour
 {
     public float _speed;
-    public PointDirection PDirec;
+    private PointDirection PDirec;
     private int PointDirectionIndex;
 
     // Start is called before the first frame update
@@ -20,6 +20,19 @@ public class EnnemiMove : MonoBehaviour
     void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, PDirec.pointsDirection[PointDirectionIndex].position, _speed * Time.deltaTime);
+        
+        if(Vector2.Distance(transform.position , PDirec.pointsDirection[PointDirectionIndex].position) < 0.1f)
+        {
+            if(PointDirectionIndex < PDirec.pointsDirection.Length - 1)
+            {
+                PointDirectionIndex++;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
     }
 
 
