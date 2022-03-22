04163f4d4cbcd589770634e9f13f5d373d9e634a
Empereur-Franc <tazi.layt@gmail.com>
2022-03-22 14:19:02 +0100

    Cooldown de tir tourelle mais c'est un peu cassé

--- ProjetTD/Assets/_003_Scripts/Tourelle.cs
+++ ProjetTD/Assets/_003_Scripts/Tourelle.cs
@@ -8,21 +8,29 @@ public class Tourelle : MonoBehaviour
     private EnnemiMove target;
     private Queue<EnnemiMove> ennemis = new Queue<EnnemiMove>();
     private bool hasTarget;
+    public float timer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = 0.5f;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         Attack();
         Debug.Log(target);
         if (hasTarget)
         {
-            Debug.Log("Prêt à tirer!");
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                Debug.Log("Prêt à tirer!");
+                Instantiate(projectile, transform.position, Quaternion.Euler(transform.right));
+                timer = 0.5f;
+            }
         }
     }
 
