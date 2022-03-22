c7762e2a4e89d4101c225b7d0c2650807659f6b6
Hugo Petruzzelli <hugo.petruzzelli25@gmail.com>
2022-03-22 13:56:56 +0100

    up coloration des menues

--- ProjetTD/Assets/_003_Scripts/Script Menu/MainMenu.cs
+++ ProjetTD/Assets/_003_Scripts/Script Menu/MainMenu.cs
@@ -3,7 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public string levelToLoad;
+    public string levelToLoad1;
+
+    public string levelToLoad2;
+
+    public string levelToLoad3;
 
     public GameObject settingsWindow;
 
@@ -11,18 +15,18 @@ public class MainMenu : MonoBehaviour
 
     public void StartLevel1()
     {
-        SceneManager.LoadScene(levelToLoad);
+        SceneManager.LoadScene(levelToLoad1);
 
     }
 
     public void StartLevel2()
     {
-        SceneManager.LoadScene(levelToLoad);
+        SceneManager.LoadScene(levelToLoad2);
     }
 
     public void StartLevel3()
     {
-        SceneManager.LoadScene(levelToLoad);
+        SceneManager.LoadScene(levelToLoad3);
     }
 
     public void Settings()
